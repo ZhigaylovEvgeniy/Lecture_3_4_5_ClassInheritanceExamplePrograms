455aac8d2c30b2bd209312927632ae1964698f78
zhigan <zhigan639@gmail.com>
2020-10-16 23:05:02 +0500

    Version 01

---
+++ src/Shape.java
@@ -0,0 +1,13 @@
+public class Shape {
+    private final Color color;
+
+    public Shape(Color color){
+        this.color = color;
+    }
+    public Color getColor(){
+        return color;
+    }
+    public double getArea(){
+        return Double.NaN;
+    }
+}
